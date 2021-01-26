918bba4cb098c67b12386cc6bce29ba68f78737d
Nate Bruggeman <njbruggeman@gmail.com>
2021-01-26 13:34:15 -0600

    added delete icon and ondelete is passed from app to taskjs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import Header from './components/Header'
+import Tasks from './components/Tasks'
+import { useState } from 'react'
+
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id:1,
+        text: 'doctors appointment',
+        day: 'Feb 4',
+        reminder: true
+    },
+    {
+        id:2,
+        text: 'interview',
+        day: 'Jan 8',
+        reminder: false,
+    },    
+    {
+        id:3,
+        text: 'family dinner',
+        day: 'March 11',
+        reminder: true,
+    }
+])
+
+  // Delete a task
+  const deleteTask = (id) => {
+    console.log('delete', id);
+  }
+
   return (
     <div className="App">
       <Header />
+      <Tasks tasks={tasks} onDelete={deleteTask}/>
     </div>
   );
 }
