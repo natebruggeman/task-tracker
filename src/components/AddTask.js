3895af528a5e4947e125aaa035ec84d1c787e490
Nate Bruggeman <njbruggeman@gmail.com>
2021-01-26 16:21:11 -0600

    reminder functioning on static data now starting addtask section

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -6,6 +6,14 @@ const AddTask = () => {
                 <label>Task</label>
                 <input type='text' placeholder='Add Task'/>
             </div>
+            <div className = 'form-control'>
+                <label>Day and Time</label>
+                <input type='text' placeholder='Add Day and Time'/>
+            </div>            
+            <div className = 'form-control'>
+                <label>Set Reminder</label>
+                <input type='checkbox'/>
+            </div>
         </form>
     )
 }
